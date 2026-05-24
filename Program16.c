805189fd7fa66d95a83123da7688b9810fbddb00
Shubham Bhapkar <sbhapkar703@gmail.com>
2026-05-24 11:56:39 +0530

    Add files via upload

---
+++ Program16.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNO)
+{
+
+    if(iNO % 2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+int main ()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+     printf("Enter number to Check Weather it is Even or Odd: ");
+     scanf("%d",&iValue);
+
+     bRet =CheckEvenOdd(iValue);
+     
+     if (bRet == true)
+     {
+        printf("%d is Even",iValue);
+
+     }
+     else{
+        printf("%d is Odd",iValue);
+     }
+     
+
+
+    return 0;
+
+}
---
+++ Program20.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int CalculateTickitPrize(int iAge)
+{
+    //input Filter
+    if(iAge<0 )
+    {
+        printf("Invalid Input");
+        return 0;
+    }
+    
+   if(iAge >= 0 && iAge <= 5)
+   {
+     return 0;
+   }
+   else if (iAge >=6 && iAge <=18)
+   {
+    return 500;
+   }
+   else if (iAge >=19 && iAge <=50)
+   {
+    return 900;
+   }
+   else
+   {
+    return 400;
+   }
+
+     
+}
+
+int main ()
+{
+    int iValue = 0;
+     int iRet=0;
+
+    printf("Please Enter your age to calculate tickit prize: \n");
+    scanf("%d",&iValue);
+   
+     iRet =CalculateTickitPrize(iValue);
+     
+   printf("Your ticket price will be :%d Rupees \n",iRet);
+
+    return 0;
+
+}
